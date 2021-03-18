eeb60a740fc5bf7f718518b952daff7d1822bb4e
Sam D <samidiaz3@gmail.com>
2021-03-18 14:44:18 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,7 +11,7 @@ class MP3Importer
 
     #Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
     files = Dir["#{path}/*.mp3"]
-    files.split("/")[-1]
+    #files.split("/")[-1]
   end
 
   def import
